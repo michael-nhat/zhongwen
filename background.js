dc0d3df17e082a2ab0e4e3e37b50ff8a7eff085b
Lien Hoang <kimlienhg@gmail.com>
2020-02-10 23:43:40 -0800

    Sanity check

--- background.js
+++ background.js
@@ -170,9 +170,9 @@ function activateExtension(tabId, showHelp) {
 
 async function loadDictData() {
     let wordDict = fetch(chrome.runtime.getURL(
-        "data/cedict_ts.u8")).then(r => r.text());
+        "data/cedict_ts_original.u8")).then(r => r.text());
     let wordIndex = fetch(chrome.runtime.getURL(
-        "data/cedict.idx")).then(r => r.text());
+        "data/cedict_original.idx")).then(r => r.text());
     let grammarKeywords = fetch(chrome.runtime.getURL(
         "data/grammarKeywordsMin.json")).then(r => r.json());
 
